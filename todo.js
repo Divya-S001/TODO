5222fc988771d363c7df4548d156a4bd9ddf610d
Divya-S001 <dsahu@trueigtech.com>
2025-02-08 17:41:07 +0530

    added local storage

--- todo.js
+++ todo.js
@@ -3,10 +3,66 @@ const inputBox=document.getElementById('inputBox');
 const btn=document.getElementById('btn');
 const list=document.getElementById('list');
 
-const addtodo =()=>(){
-    alert("ADD");
-}
+document.addEventListener("DOMContentLoaded", loadT);
+
+const addtodo =()=>{
+    const inputText= inputBox.value.trim();
+    if (inputText.length<=0){
+        alert("Please write task to add");
+            return false;
+    }
+        //list item
+    const li = document.createElement("li");   //DOM.....by this js can use html. 
+      li.innerHTML=inputText;
+   
+    //Delete button
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";  
+    deleteBtn.className = "delete-btn";
+deleteBtn.onclick = () => {li.remove();  // delete on click....
+  removeTaskfromStorage(inputText);    //remove task from LS
+};
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+    saveTaskToStorage(inputText) // save task to LS
+    inputBox.value="";
+};
+
 btn.addEventListener('click', addtodo);
+
+// save task to localStorage
+const saveTaskToStorage = (task) => {
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+//F...to load task from LS
+
+function loadT(){
+  let task=JSON.parse(localStorage.getItem("task")) || [];
+  task.forEach(task=> 
+    { const li= document.createElement("li");
+    li.innerHTML=task;
+
+      
+    const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+        deleteBtn.className = "delete-btn";
+
+        deleteBtn.onclick = () => {li.remove(); // remove task from both ui/LS
+          removeTaskFromStorage(task);
+        };
+
+        li.appendChild(deleteBtn);
+        list.appendChild(li);
+
+    
+  });
+}
+
+
+
+
 
 
 
